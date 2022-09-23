5852bb800a64f867a5137dfd547e13fe5b551da3
Nada Elkoji <nada.elkoji@student.vinci.be>
2022-09-23 09:17:50 +0200

    1.1-1.2 : refactoor-modules

---
+++ exercices/module1/1.1-1.2/src/components/Content/Content.jsx
@@ -0,0 +1,12 @@
+
+import Part from "../Part/Part";
+  const Content = (props) =>{
+    return (
+      <div>
+        <Part name={props.name1} number={props.number1}/>
+        <Part name={props.name2} number={props.number2}/>
+        <Part name={props.name3} number={props.number3}/>
+      </div>
+    )
+  };
+  export default Content;

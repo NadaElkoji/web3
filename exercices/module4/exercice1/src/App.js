9349198bc7997379329a5c99fddb64ef57a8fabd
Nada Elkoji <nada.elkoji@student.vinci.be>
2022-10-20 19:55:28 +0200

    module4: forms

---
+++ exercices/module4/exercice1/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' , number:'0484000000'}
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    persons.forEach(function (person){
+      if (person.name===newName) {
+        console.log("im here");
+        window.alert(`${newName} is already added to phonebook`)
+      } else {
+        setPersons(persons.concat(personObject))
+      setNewName('')
+      }
+    });
+
+    }
+  
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>number:
+           <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+           />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person=>
+          <l>{person.name} {person.number}<br></br></l>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
